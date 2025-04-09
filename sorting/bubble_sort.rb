79616a0bb6af65ca7b5e8e8ef4b084c17818072f
Shubham Dongarjal <shubham.dongarjal@arieotech.com>
2025-04-09 15:59:00 +0530

    Update bubble_sort.rb

--- sorting/bubble_sort.rb
+++ sorting/bubble_sort.rb
@@ -1,3 +1,17 @@
+# basic way
+arr = [22,33,42,12,23,55,34,75,35,11,5]
+
+for i in 0 ... arr.length-1 do
+  for j in 0 ... (arr.length-i-1) do
+    if(arr[j]>arr[j+1])
+      arr[j+1],arr[j] = arr[j],arr[j+1]
+    end
+  end
+end
+
+p arr
+
+# optimized way it breaks if it is already sorted
 arr = [22,33,42,12,23,55,34,75,35,11,5]
 
 for i in 0 ... arr.length-1 do
