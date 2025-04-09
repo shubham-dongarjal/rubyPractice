fbeb098399132981600567944a37464dc1867da6
Shubham Dongarjal <shubham.dongarjal@arieotech.com>
2025-04-09 15:55:18 +0530

    Create bubble_sort.rb

---
+++ sorting/bubble_sort.rb
@@ -0,0 +1,14 @@
+arr = [22,33,42,12,23,55,34,75,35,11,5]
+
+for i in 0 ... arr.length-1 do
+  swapped = false
+  for j in 0 ... (arr.length-i-1) do
+    if(arr[j]>arr[j+1])
+      arr[j+1],arr[j] = arr[j],arr[j+1]
+      swapped = true
+    end
+  end
+  break unless swapped
+end
+
+p arr
