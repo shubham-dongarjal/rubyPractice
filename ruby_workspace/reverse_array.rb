178c6450f2a33696e654dfa5777988c1b7167ca5
Shubham Dongarjal <shubham.dongarjal@arieotech.com>
2025-04-09 12:05:13 +0530

    Update reverse_array.rb

--- ruby_workspace/reverse_array.rb
+++ ruby_workspace/reverse_array.rb
@@ -1,9 +1,6 @@
-arr = [1, 2, 3, 4]
-
+1. arr = [4,6,7,2,34,8,76,45,29]
 reversed = [] 
-
 i = arr.length-1
-
 
 while i >= 0
   reversed << arr[i]
@@ -11,3 +8,7 @@ while i >= 0
 end 
 
 p reversed
+
+2. arr = [4,6,7,2,34,8,76,45,29]
+
+p arr.reverse
