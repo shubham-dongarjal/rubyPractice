2e8da6e54c83411dddaccc9d669379b67b63e6aa
Shubham Dongarjal <shubham.dongarjal@arieotech.com>
2025-04-20 22:52:58 +0530

    Update Stack.rb

--- stack_and_queue/Stack.rb
+++ stack_and_queue/Stack.rb
@@ -1,3 +1,4 @@
+# 1st way
 class Stack
 
     attr_accessor:items
@@ -50,6 +51,73 @@ stack.prints
 
 stack.empty?
 
+#2nd way
+class Stack 
+    attr_accessor:items
+
+    def initialize(size)
+        @size = size
+        # @items = Array.new(size)
+        @items = []
+        @top = -1
+    end
+
+    def push(item)
+        @top = @top + 1
+        if @top >=@size
+            puts "Stack is full! Can not push #{item}. Max size is #{@size}"
+        else
+            @items[@top] = item
+        end
+    end 
+
+    # def prints
+    #     items = []
+    #     @items.each do |a|
+    #         if a != nil
+    #             items << a 
+    #         end
+    #     end
+    #     print "#{items}\n"
+    # end
+    
+    def prints
+        print "#{@items}\n"
+    end
+
+    def len
+       puts "Size is #{@items.length}"
+    end
+
+    def pop 
+        if @top == -1
+            return puts "stack is empty"
+        end
+        puts "The value poped is #{@items.delete_at(-1)}"
+    end
+end
+
+stack = Stack.new(10)
+
+stack.push(10)
+stack.push(11)
+stack.push(12)
+stack.push(13)
+stack.push(14)
+stack.push(15)
+stack.push(12)
+stack.push(13)
+stack.push(14)
+stack.push(14)
+stack.push(14)
+
+
+
+stack.prints
+
+stack.pop
+
+stack.len
 
 
  
